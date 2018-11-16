9056a5f38c8500b00964a92e4cdfb1a165a4ba20
Michal Marciniec <michal.marciniec@gmail.com>
2018-11-16 16:09:48 +0100

    Base project structure

---
+++ src/main/java/pl/michalmarciniec/jpatraps/JpaTrapsApplication.java
@@ -0,0 +1,12 @@
+package pl.michalmarciniec.jpatraps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTrapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTrapsApplication.class, args);
+	}
+}
