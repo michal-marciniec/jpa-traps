a214baec00dd9ff7af08e265a672397d61428d12
Michal Marciniec <michal.marciniec@gmail.com>
2018-11-23 14:40:41 +0100

    Transactional test - WalletServiceTest

--- src/test/java/pl/michalmarciniec/jpatraps/requiresnew/WalletServiceTest.java
+++ src/test/java/pl/michalmarciniec/jpatraps/requiresnew/WalletServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -23,6 +24,7 @@ public class WalletServiceTest {
     private WalletService walletService;
 
     @Test
+    @Transactional
     public void shouldCreateAndAddEmptyWalletToPerson() {
         // given
         Person margaret = personRepository.save(new Person("Margaret"));
